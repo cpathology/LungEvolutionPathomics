a33f25b7fdb7eef0dfad0c1d645d3bd8af8e4fe0
PingjunChen <pingjunchen@ieee.org>
2023-11-25 19:07:56 -0600

    update value range

--- 01CellClassifier/compare_cell_properties.py
+++ 01CellClassifier/compare_cell_properties.py
@@ -35,18 +35,19 @@ if __name__ == "__main__":
     cell_fea_df = pd.concat([nyu_cell_fea_df, japan_cell_fea_df])
     sources = ["Japan",] * len(japan_cell_fea_df) + ["USA",] * len(nyu_cell_fea_df) 
     cell_fea_df["Source"] = sources
+    cell_fea_df.rename(columns={"Label": "Cell Type"}, inplace=True)
 
     # plotting
-    f, axes = plt.subplots(1, 3, figsize=(11, 6))
+    f, axes = plt.subplots(1, 3, figsize=(11, 8))
     cell_list = ["AEC", "LYM", "OC"]
-    sns.violinplot(y="Area", x="Label", hue="Source", split=True, inner=None, order=cell_list, data=cell_fea_df, orient='v', ax=axes[0])
-    sns.violinplot(y="Intensity", x="Label", hue="Source", split=True, inner=None, order=cell_list, data=cell_fea_df, orient='v', ax=axes[1])
-    sns.violinplot(y="Roundness", x="Label", hue="Source", split=True, inner=None, order=cell_list, data=cell_fea_df, orient='v', ax=axes[2])
+    sns.violinplot(y="Area", x="Cell Type", hue="Source", split=True, inner=None, order=cell_list, data=cell_fea_df, orient='v', ax=axes[0])
+    sns.violinplot(y="Intensity", x="Cell Type", hue="Source", split=True, inner=None, order=cell_list, data=cell_fea_df, orient='v', ax=axes[1])
+    sns.violinplot(y="Roundness", x="Cell Type", hue="Source", split=True, inner=None, order=cell_list, data=cell_fea_df, orient='v', ax=axes[2])
     axes[0].set_ylim(0, 1000)
-    axes[1].set_ylim(0, 200)
-    axes[2].set_ylim(0, 1.2)
+    axes[1].set_ylim(0, 240)
+    axes[2].set_ylim(0, 1.3)
 
     # save the plot
     plt.tight_layout()
     all_cell_plot_path = os.path.join(vis_dir, "cell_properites_comparison" + args.plot_format)
-    plt.savefig(all_cell_plot_path, transparent=False, dpi=300)
+    plt.savefig(all_cell_plot_path, transparent=True, dpi=300)
